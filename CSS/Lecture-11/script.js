ce603b08283b4ea7c7655881520a558124377dd6
CodeCraft Academy <codecraftacademypk@gmail.com>
2026-04-09 04:26:28 -0700

    add css lecture

---
+++ CSS/Lecture-11/script.js
@@ -0,0 +1,8 @@
+let element = document.querySelector(".container");
+document.getElementById("btn").addEventListener("click", () => {
+  if (element.style.display === "block") {
+    element.style.display = "none";
+  } else {
+    element.style.display = "block";
+  }
+});
